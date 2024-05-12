7c624eb4278c6ed35f25d236eb660765b27ed343
VladimirDoroshin <vovador03@mail.ru>
2024-05-12 15:11:17 +0300

    Add files via upload

---
+++ ntd3.py
@@ -0,0 +1,10 @@
+import json
+import matplotlib.pyplot as plt
+
+class ResultWriter:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def write_to_json(self, data):
+        with open(self.filename, 'w') as file:
+            json.dump(data, file, indent=4)
